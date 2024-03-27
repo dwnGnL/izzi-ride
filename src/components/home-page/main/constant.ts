2082ecb6f6d34679d17af0573bfc445753172f1c
AnushervonKhidirov <ankhid98@gmail.com>
2024-03-27 22:41:51 +0500

    constants added

---
+++ src/components/home-page/main/constant.ts
@@ -0,0 +1,57 @@
+import type { LineItemData } from './main'
+
+import img_0 from '@public/images/home-page/main-component/0.png'
+import img_1 from '@public/images/home-page/main-component/1.png'
+import img_2 from '@public/images/home-page/main-component/2.png'
+import img_3 from '@public/images/home-page/main-component/3.png'
+import img_4 from '@public/images/home-page/main-component/4.png'
+import img_5 from '@public/images/home-page/main-component/5.png'
+import img_6 from '@public/images/home-page/main-component/6.png'
+import img_7 from '@public/images/home-page/main-component/7.png'
+import img_8 from '@public/images/home-page/main-component/8.png'
+import img_9 from '@public/images/home-page/main-component/9.png'
+import img_10 from '@public/images/home-page/main-component/10.png'
+import img_11 from '@public/images/home-page/main-component/11.png'
+
+export const MainComponentData: LineItemData[] = [
+    {
+        copy: 'Save on your trip',
+        image: [img_1, img_9],
+    },
+    {
+        copy: 'Find a ride',
+        image: [img_2, img_3, img_4],
+    },
+    {
+        copy: 'Give a ride',
+        image: [img_8, img_11],
+    },
+    {
+        copy: 'Between cities and states',
+        image: [img_0, img_1, img_6, img_9],
+    },
+    {
+        copy: 'Easy to use',
+        image: [img_2, img_3, img_8, img_10, img_11],
+    },
+    {
+        copy: 'Communicate thru the app',
+        image: [img_3, img_4],
+    },
+    {
+        copy: 'Choose your own route',
+        image: [img_10],
+    },
+    {
+        copy: 'Choose a comfortable time',
+        image: [img_8, img_10, img_11],
+    },
+    {
+        copy: 'Choose your ridemates',
+        image: [img_1, img_5, img_7],
+    },
+    {
+        copy: 'With affordable prices',
+        image: [img_10, img_7],
+    },
+]
---
+++ src/constants/common-copy.ts
@@ -0,0 +1,1 @@
+export const MAIN_HEADLINE = `Do you often drive to another <br /> cities and states by own car <br /> or public transportation? <br /> Then save on it with iZZi RIDE`
