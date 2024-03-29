81ec83f6b81248d36b22a12964bbb2de70199df8
AnushervonKhidirov <ankhid98@gmail.com>
2024-03-29 10:18:38 +0500

    animation HOC changed for mobile and tablet

--- src/components/home-page/about/about.tsx
+++ src/components/home-page/about/about.tsx
@@ -1,4 +1,6 @@
 'use client'
+import type { ReactNode } from 'react'
+
 import Image from 'next/image'
 import AnimBlock from '@hoc/animated-block/animated-block'
 
@@ -25,19 +27,17 @@ const About = () => {
                         <div className={styles.from_block}></div>
                     </div>
                 </div>
-                <h2 className={styles.headline}>Choose a route and time</h2>
-                <div className={styles.copy}>
-                    Our app is designed to make it easy and affordable for you to travel around the
-                    cities and states. We have developed a user-friendly interface for finding and
-                    connecting drivers and ridemates and communication between them thru the app.
-                </div>
+
+                <Headline>Choose a route and time</Headline>
+                <Copy>
+                    Our app is designed to make it easy and affordable for you to travel around the cities and states.
+                    We have developed a user-friendly interface for finding and connecting drivers and ridemates and
+                    communication between them thru the app.
+                </Copy>
             </AnimBlock>
 
             <AnimBlock className={styles.about} inViewExtraClass={styles.in_view}>
-                <div
-                    className={`${styles.media} ${styles.video}`}
-                    data-title='Why you should use iZZi RIDE?'
-                >
+                <div className={`${styles.media} ${styles.video}`} data-title='Why you should use iZZi RIDE?'>
                     <iframe
                         src={`${videoUrl}?si=is6dbmK1A1XTpGdO&rel=0`}
                         title='Why you should use iZZi RIDE?'
@@ -45,20 +45,42 @@ const About = () => {
                     ></iframe>
                 </div>
 
-                <h2 className={styles.headline}>Why you should use iZZi RIDE?</h2>
+                <Headline>Why you should use iZZi RIDE?</Headline>
 
-                <div className={styles.copy}>
-                    With the app, you can personalize and customize your trip, paying less than for
-                    a regular taxi as a passenger or save on gas and tolls by sharing the costs
-                    with your ridemates as a driver on your own car. Soon the application will have
-                    a smart system for evaluating the drivers and ridemates thru the app. Both sides
-                    can share their experience with others and rate their trip on the app. It will
-                    give others more information before booking a ride and keep their travel more
-                    safe and comfortable.
-                </div>
+                <Copy>
+                    With the app, you can personalize and customize your trip, paying less than for a regular taxi as a
+                    passenger or save on gas and tolls by sharing the costs with your ridemates as a driver on your own
+                    car. Soon the application will have a smart system for evaluating the drivers and ridemates thru the
+                    app. Both sides can share their experience with others and rate their trip on the app. It will give
+                    others more information before booking a ride and keep their travel more safe and comfortable.
+                </Copy>
             </AnimBlock>
         </section>
     )
+}
+
+const Headline = ({ children }: { children: ReactNode }) => {
+    if (getDeviceType() === 'desktop') {
+        return <h2 className={styles.headline}>{children}</h2>
+    } else {
+        return (
+            <AnimBlock>
+                <h2 className={styles.headline}>{children}</h2>
+            </AnimBlock>
+        )
+    }
+}
+
+const Copy = ({ children }: { children: ReactNode }) => {
+    if (getDeviceType() === 'desktop') {
+        return <div className={styles.copy}>{children}</div>
+    } else {
+        return (
+            <AnimBlock>
+                <div className={styles.copy}>{children}</div>
+            </AnimBlock>
+        )
+    }
 }
 
 export default About
