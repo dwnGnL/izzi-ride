0a1fb62c1ce98cfe87b1d5af0f8d7c690258cfdb
AnushervonKhidirov <ankhid98@gmail.com>
2024-04-17 10:25:01 +0500

    form changed

--- src/components/common/form/form.tsx
+++ src/components/common/form/form.tsx
@@ -1,65 +1,95 @@
-import type { FC, FormEvent } from 'react'
+import type { FC, FormEvent, MouseEventHandler } from 'react'
 import type { Form, Input } from '@type/form.type'
 
+import Select from '@common/select/select'
 import Button from '@common/button/button'
 
 import styles from './form.module.css'
 
 const Form: FC<Form> = ({ endpoint, inputs, buttonText, className, children }) => {
-	async function onSubmit(e: FormEvent<HTMLFormElement>) {
-		e.preventDefault()
-		const formData = new FormData(e.currentTarget)
-		const body = Object.fromEntries(formData)
+    async function onSubmit(e: FormEvent<HTMLFormElement>) {
+        e.preventDefault()
+        const formData = new FormData(e.currentTarget)
+        const body = Object.fromEntries(formData)
 
-		try {
-			const response = await fetch(endpoint, {
-				method: 'POST',
-				body: JSON.stringify(body),
-				headers: {
-					'Content-Type': 'application/json',
-				},
-			})
+        try {
+            const response = await fetch(endpoint, {
+                method: 'POST',
+                body: JSON.stringify(body),
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            })
 
-			if (!response.ok) throw new Error('whoops')
+            if (!response.ok) throw new Error('whoops')
 
-			const data = await response.json()
+            const data = await response.json()
 
-			console.log(data)
-		} catch (err: any) {
-			console.log(err)
-		}
-	}
+            console.log(data)
+        } catch (err: any) {
+            console.log(err)
+        }
+    }
 
-	return (
-		<form onSubmit={onSubmit} className={`${styles.form} ${className}`}>
-			<div>
-				{inputs.map(input => (
-					<Input
-						key={input.name}
-						type={input.type}
-						name={input.name}
-						placeholder={input.placeholder}
-						required={input.required}
-					/>
-				))}
+    return (
+        <form onSubmit={onSubmit} className={`${styles.form} ${className}`}>
+            {/* <div>
+                {inputs.map(input => (
+                    <Input
+                        key={input.name}
+                        type={input.type}
+                        name={input.name}
+                        placeholder={input.placeholder}
+                        required={input.required}
+                    />
+                ))}
 
-				{children && children}
-			</div>
+                {children && children}
+            </div> */}
 
-			<Button title={buttonText} type='submit' className={styles.submit_btn} />
-		</form>
-	)
+            <div>
+                {inputs.map(input => (
+                    <Field
+                        key={input.name}
+                        type={input.type}
+                        name={input.name}
+                        title={input.title}
+                        placeholder={input.placeholder}
+                        options={input.options}
+                        required={input.required}
+                    />
+                ))}
+
+                {children && children}
+            </div>
+
+            <Button title={buttonText} type='submit' className={styles.submit_btn} />
+        </form>
+    )
 }
 
-const Input: FC<Input> = ({ type, name, placeholder, required }) => {
-	const placeholderText = required ? `${placeholder}*` : placeholder
+const Field: FC<Input> = ({ type, name, title, placeholder, required, options }) => {
+    const fieldTitle = required ? `${title} (required)` : title
+    let field = null
 
-	return (
-		<label>
-			<div className={styles.placeholder}>{placeholderText}</div>
-			<input type={type} name={name} required={required} />
-		</label>
-	)
+    if (type === 'select' && options) {
+        field = <Select type={type} name={name} title={title} options={options} />
+    } else if (type === 'textarea') {
+        field = <textarea name={name} required={required} placeholder={placeholder}></textarea>
+    } else {
+        field = <input type={type} name={name} required={required} placeholder={placeholder} />
+    }
+
+    function labelHandler(e: any) {
+        if (type === 'select') e.preventDefault()
+    }
+
+    return (
+        <label onClick={(e) => labelHandler(e)} className={styles.label}>
+            <div className={styles.field_title}>{fieldTitle}</div>
+            {field}
+        </label>
+    )
 }
 
 export default Form
--- src/components/home-page/contact-us/constant.ts
+++ src/components/home-page/contact-us/constant.ts
@@ -1,20 +1,37 @@
 import type { Input } from '@type/form.type'
 
 export const inputs: Input[] = [
-	{
-		type: 'text',
-		name: 'name',
-		placeholder: 'Name',
-	},
-	{
-		type: 'number',
-		name: 'phone',
-		placeholder: 'Phone number',
-	},
-	{
-		type: 'email',
-		name: 'email',
-		placeholder: 'Email',
-		required: true,
-	},
+    {
+        type: 'text',
+        name: 'name',
+        title: 'Name',
+        placeholder: 'John Doe',
+        required: true,
+    },
+    {
+        type: 'email',
+        name: 'email',
+        title: 'Email',
+        placeholder: 'youtemail@gmail.com',
+        required: true,
+    },
+    {
+        type: 'number',
+        name: 'phone',
+        title: 'Phone number',
+        placeholder: '+15323425343',
+    },
+    {
+        type: 'select',
+        name: 'subject',
+        title: 'Subject',
+        required: true,
+        options: ['Join the Beta', 'Bug', 'Advertising', 'Suggest'],
+    },
+    {
+        type: 'textarea',
+        name: 'message',
+        title: 'Message',
+        placeholder: 'Write you message here',
+    },
 ]
