2d15b9279953696ebb9b6e4aa2ea10e01a5188be
AnushervonKhidirov <ankhid98@gmail.com>
2024-04-19 10:41:35 +0500

    name fixed

--- src/components/home-page/slider/constant.ts
+++ src/components/home-page/slider/constant.ts
@@ -7,13 +7,13 @@ export const owners: Slide[] = [
     {
         image: shukhrat,
         about: "<p>Many years ago, when I was in Europe, one of my friends showed me a similar application. Almost everyone in Europe uses the same way of ride sharing and saves on their trip costs.</p><p>When I used it for the first time, I realized how comfortable, safe, and affordable ride sharing can be. A couple of years ago, I moved to the USA and was surprised to see people driving to another city or state alone with empty seats in their cars and not sharing their expenses and costs on high gas prices and tolls with others who need to go to the same place. It's also complicated for people without cars to travel to other cities and states, and there aren't many public transportations with affordable prices and comfortable departure times.</p><p>This inspired me to create an app that connects drivers and passengers who plan to go to the same place and makes it easy for them. I contacted my cousin, Barzu Rustami, who is a software developer, and discussed my idea with him. He agreed to help me with my idea. This app gives everyone in the USA the opportunity to make their trips more comfortable, easy, and profitable. I believe iZZi RIDE app will help others save on their trips and make them more comfortable.</p>",
-        name: 'Safarov Shukhrat',
+        name: 'Shukhrat Safarov',
         position: 'Founder',
     },
     {
         image: barzu,
         about: "<p>As a co-founder and developer of the app, I bring a deep passion for the product and a strong technical background to the team. I am excited to be a part of creating a product that will help people in a meaningful way.</p><p>I have always been interested in technology and its potential to solve problems. I started coding at a young age and have been building software ever since. I am passionate about creating user-friendly and intuitive products that make people's lives easier.</p>",
-        name: 'Rustami Barzu',
+        name: 'Barzu Rustami',
         position: 'Co-founder',
     },
 ]
