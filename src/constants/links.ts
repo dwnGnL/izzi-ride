e624059ffe3bf0c4d8ebd439ee4126395824ec9c
AnushervonKhidirov <ankhid98@gmail.com>
2024-08-03 10:41:51 +0500

    google play link updated

--- src/constants/links.ts
+++ src/constants/links.ts
@@ -1,5 +1,5 @@
 export const APP_STORE_URL = 'https://apps.apple.com/us/app/izzi-ride/id6449208978'
-export const GOOGLE_PLAY_URL = 'https://t.me/izziride'
+export const GOOGLE_PLAY_URL = 'https://play.google.com/store/apps/details?id=com.easyride.ezride'
 export const CONTACT_US_ENDPOINT = 'https://ezride.pro/api/v1/client/complaint'
 
 // pages
