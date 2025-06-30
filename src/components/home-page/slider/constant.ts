859eb9acb36479d9f09f2f43e9ca8ac9219430e8
Anushervon Khidirov <ankhid98@gmail.com>
2025-06-30 16:28:07 +0500

    new people added

--- src/components/home-page/slider/constant.ts
+++ src/components/home-page/slider/constant.ts
@@ -2,6 +2,9 @@ import type { Slide } from './slider'
 
 import shukhrat from '@public/images/owners/shukhrat.jpg'
 import barzu from '@public/images/owners/barzu.jpg'
+import farrukh from '@public/images/owners/farrukh.jpg'
+import parvona from '@public/images/owners/parvona.jpg'
+import vladimir from '@public/images/owners/vladimir.jpg'
 
 export const owners: Slide[] = [
     {
@@ -15,5 +18,23 @@ export const owners: Slide[] = [
         about: "<p>As a co-founder and developer of the app, I bring a deep passion for the product and a strong technical background to the team. I am excited to be a part of creating a product that will help people in a meaningful way.</p><p>I have always been interested in technology and its potential to solve problems. I started coding at a young age and have been building software ever since. I am passionate about creating user-friendly and intuitive products that make people's lives easier.</p>",
         name: 'Barzu Rustami',
         position: 'Co-founder',
+    },
+    {
+        image: farrukh,
+        about: "<p>- Advisor, Local Growth – (New York area)</p> <p>Seasoned QA engineer with experience at IEEE and Citizens Bank. Advises iZZi Ride on local user acquisition, community growth and early-stage brand development in NYC.</p>",
+        name: 'Farrukh Zaripov',
+        position: 'iZZi Ride City Lead – New York',
+    },
+    {
+        image: vladimir,
+        about: "<p>- Advisor, User Acquisition – (Northeast - USA area, Remote)</p> <p>Seasoned tech entrepreneur and executive with deep expertise in programmatic advertising, mobile app growth, and product management. As CEO & Co‑Founder of hybe.io, he led development of a scalable adtech platform designed for performance-driven mobile user acquisition   . Prior to that, he managed product strategy and business development across roles at hybrid.ai and Kayzen, overseeing digital marketing, analytics, and go‑to‑market operations.</p> <p>At iZZi Ride, Vladimir supports Northeast market expansion on a pro bono basis — advising on adtech-led user acquisition tactics, optimizing marketing funnels, and shaping data-driven growth initiatives to accelerate driver and rider onboarding in the city.</p>",
+        name: 'Vladimir Khudiakov',
+        position: 'Advisor User Acquisition',
+    },
+    {
+        image: parvona,
+        about: "<p>- Student Ambassador & Growth Advisor (Boston area, Remote)</p> <p>Parvona is currently completing an internship at Harvard University. With strong ties to the student community and firsthand experience of campus life in the U.S., she plays a key role in promoting iZZi Ride among university students in the Boston area. Her focus is on organic user acquisition, especially among young and mobile audiences, by leveraging peer networks and student platforms at top schools like Harvard and MIT.</p>",
+        name: 'Parvona Abdurakhmanova ',
+        position: 'iZZi Ride City Lead – Boston',
     },
 ]
